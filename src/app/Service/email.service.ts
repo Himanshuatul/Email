6a6ecf03d8717b6fa2695393280522c07345ba74
Himanshuatul <himrsmaurya@gmail.com>
2024-08-24 14:08:35 +0530

    second

--- src/app/Service/email.service.ts
+++ src/app/Service/email.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class EmailService {
-  private baseurl: string = "http://localhost:8080";
+  private baseurl: string = "emailapi-production-37bd.up.railway.app";
 
   constructor(private http: HttpClient) {}
 
